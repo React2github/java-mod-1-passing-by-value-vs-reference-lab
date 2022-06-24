e99cc0fd61a4fa7d1e4f506d9f51581263fd7b43
Abdella <abdella.shukur2@gmail.com>
2022-06-23 23:51:17 -0400

    solution for the lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+    public class NumberContainer {
+        public int number;
+    }
     public static void main(String[] args) {
-        // your code here
+        NumberContainer myNumber = NumberContainer();
+        multiplyNumbers(myNumber,12,3);
+        System.out.println("Result = " + myNumber.number);
+    }
+    
+    private static Main.NumberContainer NumberContainer() {
+        return null;
+    }
+
+    public static void multiplyNumbers(NumberContainer result, int firstNumber, int secondNumber) {
+        result.number = firstNumber * secondNumber;
     }
 }
+
+
